c3b0b69233138086daea2dc23954e1e57aa0439f
Polytech <Polytech@admins-iMac.local>
2018-02-27 08:27:50 +0100

    add RendezVous et RendezVousSEt

---
+++ ParkinCare/RendezVousSet.swift
@@ -0,0 +1,28 @@
+//
+//  RendezVousSet.swift
+//  ParkinCare
+//
+//  Created by Polytech on 27/02/2018.
+//  Copyright © 2018 Kopezaur. All rights reserved.
+//
+
+import Foundation
+
+// ----- Specifications fonctionnelles
+//addRendezVous: RendezVousSet x RendezVous -> RendezVousSet
+//Ajoute un rendez vous aux autres rendez vous
+//
+//delRendezVous: RendezVousSet x RendezVous -> RendezVousSet
+//Pre : Le rendez vous en parametre doit etre present dans le RendezVousSet sinon ne change rien
+//Enleve des rendez vous le rendez vous entre en parametre
+//
+//getFutureRendezVous: RendezVousSet -> RendezVousSet
+//Renvoie les rendez vous dont la date est après la date d’aujourd’hui
+//
+//makeIterator: RendezVousSet -> ItRendezVous
+//Cree un iterateur pour RendezVousSet
+
+
+class RendezVousSet{
+    
+}
