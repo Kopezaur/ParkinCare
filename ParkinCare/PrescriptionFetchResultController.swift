49321401a0e9a57e14ad482995b6f19aca076b4d
Unknown <fabien.turgut@etu.umontpellier.fr>
2018-04-02 21:34:00 +0200

    Pilulier first view

---
+++ ParkinCare/PrescriptionFetchResultController.swift
@@ -0,0 +1,9 @@
+//
+//  PrescriptionFetchResultController.swift
+//  ParkinCare
+//
+//  Created by julia on 02/04/2018.
+//  Copyright © 2018 Kopezaur. All rights reserved.
+//
+
+import Foundation
---
+++ ParkinCare/PrescriptionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PrescriptionTableViewCell.swift
+//  ParkinCare
+//
+//  Created by julia on 02/04/2018.
+//  Copyright © 2018 Kopezaur. All rights reserved.
+//
+
+import UIKit
+
+class PrescriptionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
