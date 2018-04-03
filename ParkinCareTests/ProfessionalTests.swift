e8eb071d30f91b845f9e2e571b86b3af38af1a76
Stefan-Dragos COPETCHI <stefan-dragos.copetc@igtp3-mac12-2.isim.intra>
2018-04-03 18:31:34 +0200

    Finalisation of the implementation of Professional's Tests Unitaire

--- ParkinCareTests/ProfessionalTests.swift
+++ ParkinCareTests/ProfessionalTests.swift
@@ -19,16 +19,21 @@ class ProfessionalTests: XCTestCase {
     
     var title2 : Title!
     
+    var profSet : ProfessionalSet!
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         super.setUp()
         // Initiate titles
-        title1 = Title(name: "Neurologue")
-        title2 = Title(name: "Psychologue")
+        title1 = Title(name: "TitleTest1")
+        title2 = Title(name: "TitleTest2")
         
         // Initiate professionals
-        prof1 = Professional(lastname: "Copetchi", firstname: "Stefan", title: title1, organization: "Polytech Bucharest", email: "stefan-dragos.copetchi@etu.umontpellier.fr", numTel: "13")
-        prof2 = Professional(lastname: "Turgut", firstname: "Fabien", title: title2, organization: "Polytech Montpellier", email: "fabien.turgut@etu.umontpellier.fr", numTel: "007")
+        prof1 = Professional(lastname: "Test", firstname: "Stefan", title: title1, organization: "Polytech Bucharest", email: "stefan-test@etu.umontpellier.fr", numTel: "13")
+        prof2 = Professional(lastname: "Test", firstname: "Fabien", title: title2, organization: "Polytech Montpellier", email: "fabien-test@etu.umontpellier.fr", numTel: "007")
+        
+        // Initiate the professional set
+        profSet = ProfessionalSet()
         
     }
     
@@ -36,35 +41,72 @@ class ProfessionalTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
         // Tear down the titles
-        title1 = nil
-        title2 = nil
+        TitleDAO.delete(title: title1)
+        TitleDAO.delete(title: title2)
+        self.title1 = nil
+        self.title2 = nil
         
         // Tear down the professionals
-        prof1 = nil
-        prof2 = nil
+        ProfessionalDAO.delete(professional: prof1)
+        ProfessionalDAO.delete(professional: prof2)
+        self.prof1 = nil
+        self.prof2 = nil
+        
+        // Tear down the professional set
+        self.profSet = nil
         
     }
     
-    func testExistenceInCoreData() {
-        // Tests for first professional
+    func testProfessionalExtension() {
+        // Tests for the first professional
         XCTAssertNoThrow(Professional.count(professional: prof1))
         XCTAssertNoThrow(Professional.search(professional: prof1))
         
         // Tests for the second professional
-        XCTAssertNoThrow(Professional.count(professional: prof2), "Test(Count prof2)")
-        XCTAssertNoThrow(Professional.search(professional: prof2), "Test")
-        
+        XCTAssertNoThrow(Professional.count(professional: prof2), "Test(Count for prof2)")
+        XCTAssertNoThrow(Professional.search(professional: prof2), "Test(Search for prof2)")
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testDAO() {
+        // Test the DAO functions
+        XCTAssertNoThrow(ProfessionalDAO.count)
+        
+        XCTAssertNoThrow(ProfessionalDAO.fetchAll())
+        
+        XCTAssertNoThrow(ProfessionalDAO.count(professional: prof1))
+        
+        XCTAssertNoThrow(ProfessionalDAO.add(professional: prof1))
+        
+        XCTAssertNoThrow(ProfessionalDAO.delete(professional: prof1))
+    }
+    
+    func testProfessionalSet() {
+        // Test the professional set functions
+        XCTAssertNoThrow(profSet.makeIterator())
+
+        XCTAssertNoThrow(profSet.add(professional: prof1))
+
+        XCTAssertNoThrow(profSet.add(professional: prof2))
+
+        XCTAssertNoThrow(profSet.count)
+
+        XCTAssertTrue(profSet.contains(prof2))
+
+        XCTAssertNoThrow(profSet.indexOf(professional: prof2))
+        
+        XCTAssertNoThrow(profSet.remove(professional: prof2))
+        
+        XCTAssertNoThrow(profSet.remove(professional: prof1))
+        
+        XCTAssertFalse(profSet.contains(prof2))
+        
     }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
+            let _ = ProfessionalDAO.fetchAll()
         }
     }
     
